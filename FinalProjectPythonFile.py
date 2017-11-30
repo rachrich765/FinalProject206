c6f57f6095a047fff3685e3c76b8806ba57474df
Rachel Richardson <rachjr@umich.edu>
2017-11-30 14:54:29 -0500

    everything works except for the location parameter

--- FinalProjectPythonFile.py
+++ FinalProjectPythonFile.py
@@ -1,9 +1,6 @@
 ## SI 206 2017
 ## Project 4
 
-
-
-
 import operator
 import unittest
 import itertools
@@ -13,26 +10,23 @@ import sqlite3
 import sys
 ## Your name:Rachel Richardson
 
-
-#####
-
-##### TWEEPY SETUP CODE:
-# Authentication information should be in ..
 import requests
 from pprint import pprint
-my_id = '4WXJB5FPM1JZWXLPM1JXI10JQVUYWXR4ZXTQCP5C4BHP0RCK'
-my_secret = 'UNM3JUBSXQJ1QJYIOHNYGWRC2HAPLTNP0GPQX3W023OVXGBO'
+
+#get places rating from FourSquare
+my_id_foursquare = '4WXJB5FPM1JZWXLPM1JXI10JQVUYWXR4ZXTQCP5C4BHP0RCK'
+my_secret_foursquare = 'UNM3JUBSXQJ1QJYIOHNYGWRC2HAPLTNP0GPQX3W023OVXGBO'
 base_url = "https://api.foursquare.com/v2/venues/search?query="
 given_place = input('enter place: ')
 near_city = input('enter city: ')
-params = dict(client_id= my_id, client_secret= my_secret,
+params1 = dict(client_id= my_id_foursquare, client_secret= my_secret_foursquare,
  v='20170801', near = near_city, limit=1)
-url = base_url + given_place + "&intent=browse"
-resp = requests.get(url=url, params=params)
-data = json.loads(resp.text)
-id_place = data['response']['venues'][0]['id']
-#id_place = data['response']['venues'][0]['id']
-#get likes, hereNow, popular hours, name
+url1 = base_url + given_place + "&intent=browse"
+resp1 = requests.get(url=url1, params=params1)
+data1 = json.loads(resp1.text)
+id_place = data1['response']['venues'][0]['id']
+id_place = data1['response']['venues'][0]['id']
+
 CACHE_FNAME = "206_FinalProject_cache.json"
 ## either gets new data or caches data, depending upon what the input
 ##		to search for is.
@@ -54,14 +48,14 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     else:
         f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
         print(*map(f, objects), sep=sep, end=end, file=file)
-# Define your function get_user_tweets here:
+# # Define your function get_user_tweets here:
 def get_place_info(place, city):
     if place in CACHE_DICTION:
         place_results = CACHE_DICTION[place]
     else:
         base_url2 =  'https://api.foursquare.com/v2/venues/'
         url2 = base_url2 + id_place
-        params2 = dict(client_id= my_id, client_secret= my_secret,
+        params2 = dict(client_id= my_id_foursquare, client_secret= my_secret_foursquare,
          v='20170801', limit = 1)
         data_places = requests.get(url=url2, params=params2)
         place_results = json.loads(data_places.text)
@@ -85,7 +79,7 @@ for x in place_stuff['response']['venue']['tips']['groups'][0]['items']:
 
 venue_id = place_stuff['response']['venue']['id']
 venue_name = place_stuff['response']['venue']['name']
-venue_full_address = place_stuff['response']['venue']['location']['formattedAddress']
+venue_full_address = str(place_stuff['response']['venue']['location']['formattedAddress'])
 rating = place_stuff['response']['venue']['rating']
 most_popular_tip = sorted_text_and_agree_counts[0][0]
 agree_count_of_most_agreed = sorted_text_and_agree_counts[0][1]
@@ -102,34 +96,53 @@ days_and_popular_times_dict = dict(days_and_popular_times_dict)
 
 uprint(venue_id)
 uprint(venue_name)
-uprint(venue_full_address)
+uprint(str(venue_full_address))
 uprint(rating)
 uprint(most_popular_tip)
 uprint(agree_count_of_most_agreed)
 uprint(people_there_now)
-
-conn = sqlite3.connect('FinalProject.sqlite')
-cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS Places")
-cur.execute("CREATE TABLE Places (place_id TEXT, name TEXT, description TEXT, location TEXT)")
-
-# for tw in place_stuff:
-#     print(tw)
-    #tup = tw['response']['venue']['id'], tw['response']['venue']['name'], tw['response']['venue']['description'], tw['response']['venue']['location']['city']
-    #cur.execute("INSERT INTO Users (place_id, name, description, location) VALUES (?, ?, ?, ?)", tup)
-conn.commit()
-conn = sqlite3.connect('206_APIsAndDBs.sqlite')
-cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS Reviews')
-cur.execute("CREATE TABLE Reviews (name TEXT, rating NUMBER, review TEXT, agreements NUMBER)")
+uprint(days_and_popular_times_dict)
+#
+# #creating tables
+# conn = sqlite3.connect('FinalProject.sqlite')
+# cur = conn.cursor()
+# cur.execute("DROP TABLE IF EXISTS Places")
+# cur.execute("CREATE TABLE Places (place_id TEXT, name TEXT, description TEXT, location TEXT)")
+#
 # # for tw in place_stuff:
-# #     tup = tw['response']['venue']['name']
-# #sorted_text_and_agree_counts[0], tw['response']['venue']['rating'], tw['response']['venue']['tips']['groups'][0]['items'][0]['agreeCount']
-# #     cur.execute("INSERT INTO Tweets (name, review, user_posted, agreements) VALUES (?, ?, ?, ?)", tup)
-conn.commit()
-cur.execute('DROP TABLE IF EXISTS Popularity')
-cur.execute("CREATE TABLE Popularity (name TEXT, popular_hours NUMBER, here_now NUMBER)")
+# #     print(tw)
+#     #tup = tw['response']['venue']['id'], tw['response']['venue']['name'], tw['response']['venue']['description'], tw['response']['venue']['location']['city']
+#     #cur.execute("INSERT INTO Users (place_id, name, description, location) VALUES (?, ?, ?, ?)", tup)
+# conn.commit()
+# conn = sqlite3.connect('206_APIsAndDBs.sqlite')
+# cur = conn.cursor()
+# cur.execute('DROP TABLE IF EXISTS Reviews')
+# cur.execute("CREATE TABLE Reviews (name TEXT, rating NUMBER, review TEXT, agreements NUMBER)")
+# # # for tw in place_stuff:
+# # #     tup = tw['response']['venue']['name']
+# # #sorted_text_and_agree_counts[0], tw['response']['venue']['rating'], tw['response']['venue']['tips']['groups'][0]['items'][0]['agreeCount']
+# # #     cur.execute("INSERT INTO Tweets (name, review, user_posted, agreements) VALUES (?, ?, ?, ?)", tup)
+# conn.commit()
+# cur.execute('DROP TABLE IF EXISTS Popularity')
+# cur.execute("CREATE TABLE Popularity (name TEXT, popular_hours NUMBER, here_now NUMBER)")
 #for tw in place_stuff:
 #     tup = tw['response']['venue']['popular'], tw['response']['venue']['hereNow']['count'], tw['response']['venue']['name']
-#     cur.execute("INSERT INTO Tweets (name, here_now, popular_hours) VALUES (?, ?, ?)", tup)
-conn.commit()
+# #     cur.execute("INSERT INTO Tweets (name, here_now, popular_hours) VALUES (?, ?, ?)", tup)
+# #
+#get lattitude and longitude for data in cache so that Google Places API may be used
+google_geocoding_api_key = 'AIzaSyDGFEb0ZfWRf0VdLPJ5NEk_7Rv8gfXJMfw'
+url3 = 'https://maps.googleapis.com/maps/api/geocode/json?'
+params3 = dict(key = google_geocoding_api_key, address = venue_full_address)
+resp3 = requests.get(url=url3, params=params3)
+data3 = json.loads(resp3.text)
+lat_address = str(data3['results'][0]['geometry']['location']['lat'])
+long_address = str(data3['results'][0]['geometry']['location']['lng'])
+uprint(lat_address, long_address)
+# #get rating from Google Places API in order to compare it to rating on FourSquare
+google_places_key = 'AIzaSyB5lhcP3c953-H5wnwuel5o8cS33MFLMFE'
+url4 = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+params4= dict(key= google_places_key, location = '41.9428366, -87.64906129999997', rankby = 'distance', keyword = given_place, page_token = 1)
+resp4 = requests.get(url=url4, params=params4)
+data4 = json.loads(resp4.text)
+uprint(data4['results'][0]['rating'])
+#(lat_address,long_address)
